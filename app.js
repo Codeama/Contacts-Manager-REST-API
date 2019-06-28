f6fe86a93f8d6f7433ae97bbef1abcbe24fd99bd
Codeama <bukola_jimoh@yahoo.co.uk>
2019-06-28 11:01:30 +0100

    testing on heroku ...

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ console.log("Config:", config)
 
 //Cron job
 console.log('Before job instantiation');
-const job = new CronJob('0 12 * * *', function() {
+const job = new CronJob('6 10 * * *', function() {
 	const date = new Date();
   console.log('Every day at 6 AM:', date);
   checkBirthday();
